11beecb5c4060bdb2fa55ce7f77e0893bdd429aa
sudosiyang <744276721@qq.com>
2014-02-26 15:08:22 +0800

    add

--- data/data.js
+++ data/data.js
@@ -11,7 +11,7 @@ var Data = {
 		js: '/**\n* @parent 放置插件的容器\n* @current 当前页\n* @item  每页显示数量\n* @total  数据总数\n* @display_num 按钮最多显示数量\n* @selectChange 选项更改时触发的事件\n**/\nseajs.use("lib/util/pager/page",function(pager){\n  pager.init({\n		parent: ".pager",\n		current: 1,\n		item: 10,\n		total: 270,\n		display_num:5,\n		selectChange: function() {\n			console.log(this.current);\n		}\n	});\n})'
 	},
 	dialog: {
-		html: "<div class='con' style='display:none'>这是一个测试，这是抓取过来的隐藏DOM</div>\n<script src='"+path+"'></script>",
+		html: "<div class='con' style='display:none'>\n    这是一个测试，这是抓取过来的隐藏DOM\n</div>\n<script src='"+path+"'></script>",
 		css: "",
 		js: '/*!Extend jquery.js*/\n/**\n* @title 标题内容\n* @content 如果传入的是HTMLElement类型，\n		   如果是隐藏元素会给其设置display:block以显示该元素，\n		   其他属性与绑定的事件都会完整保留\n* @ok 	 确定按钮回调函数。\n		 函数如果返回false将阻止对话框关闭；函数this指针指向内部api；\n		 如果传入true表示只显示有关闭功能的按钮\n* @okVal  "确定按钮"文字\n* @cancel 取消按钮回调函数。\n		  函数如果返回false将阻止对话框关闭；函数this指针指向内部api；如果传入true表示只显示有关闭功能的按钮\n	      对话框标题栏的关闭按钮其实就是取消按钮，只不过视觉不同罢了，点击同样触发cancel事件\n* @cancelVal "取消按钮"文字\n* @copy   复制按钮，复制文字\n* @copy_target 复制目标默认.textarea\n* @width   设置消息内容宽度，可以带单位。一般不需要设置此，对话框框架会自己适应内容。\n		   如果设置为百分值单位，将会以根据浏览器可视范围作为基准，此时如果浏览器窗口大小被改变其也会进行相应的调整\n* @height  设置消息内容高度，可以带单位。不建议设置此，而应该让内容自己撑开高度。\n		   如果设置为百分值单位，将会以根据浏览器可视范围作为基准，此时如果浏览器窗口大小被改变其也会进行相应的调整\n* @drag   是否允许用户拖动位置\n* @time   设置对话框显示时间。以毫秒为单位\n*/\nseajs.use("lib/util/dialog/dialog",function(pop){\n  pop.dialog({\n    title:"这是一个测试",\n    content:$(".con"),\n    ok:function(){\n  		alert();\n  	},\n    drag:true,\n    copy:function(){\n    	this.tip();\n    },\n    copy_target:".con",\n  })	\n})'
 	},
@@ -59,5 +59,10 @@ var Data = {
 		html:"<div class='temp'></div>\n<div class='result'></div>\n<script src='"+path+"'></script>",
 		css:"",
 		js:'/**\n* str 模板\n* data 数据\n**/\nseajs.use("lib/base/util",function(util){\n  var str = "<p>房间名称：%%</br>房间人数：%%</p>";\n  var value = util.stringCat(str,3,4);\n  $(".temp").append(value).next().text(value);\n});'
+	},
+	cookie:{
+		html:"<div class='cookie'></div>\n<script src='"+path+"'></script>",
+		css:"",
+		js:'/**\n* cookie创建，获取\n**/\nseajs.use("lib/util/cache/cookie",function(pager){\n  $.cookie("TEJ","easy");\n  $.cookie("TEJ","better",{"expires":1,"path":"/","domain":""});\n  $(".cookie").text($.cookie("TEJ"));\n  /**\n  * cookie删除\n  **/\n  //$.removeCookie("TEJ");\n})\n'
 	}
 }
